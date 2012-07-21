d465c5a96cdb0f0c097689be9c6a119cfa176e32
charlzbryan <charlzbryan@gmail.com>
2012-07-21 13:32:54 +0530

    Minor Edit - Remove comments

--- SaveFile.js
+++ SaveFile.js
@@ -1,6 +1,4 @@
-// A generic onclick callback function.
 function imageOnClick(info, tab) {
-	//alert(JSON.stringify(info));
 	chrome.experimental.downloads.download({"url" : info.srcUrl,"saveAs":false});
 }
   var id = chrome.contextMenus.create({"title": "Save Image to Downloads","contexts":["image"],"onclick": imageOnClick});
